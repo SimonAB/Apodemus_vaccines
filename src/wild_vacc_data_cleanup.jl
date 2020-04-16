511cd319db3d405870d62949a779c8af2d7db15f
SimonAB <s.babayan@gmail.com>
2020-04-16 11:50:48 +0100

    some data sanity checks

--- src/wild_vacc_data_cleanup.jl
+++ src/wild_vacc_data_cleanup.jl
@@ -1,5 +1,11 @@
 using DataFrames, Query
 using CSV
+using StatsBase
 
 # Import data
 raw_data = CSV.read("./data/elisa_data_subset_for_analysis.csv"; missingstrings=["NA"], pool=true, copycols=true)
+
+# Check data
+describe(raw_data)
+countmap(raw_data.sex)
+proportionmap(raw_data.sex)
