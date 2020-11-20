f0854a6d38e44a8a939f1d525892d67eb0986d0a
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2020-11-20 13:46:31 +0000

    Update wild_vacc_analysis.jl

--- src/wild_vacc_analysis.jl
+++ src/wild_vacc_analysis.jl
@@ -23,9 +23,7 @@ lab =
 #test independences of current DAG
 @rput lab
 m1 = lm(@formula(Weight ~ days_since_1st_D_inj), lab)
-m2 = lm(@formula(Weight ~ Fat_Scores_Sum + Diet + Sex), lab)
-m3 = lm(@formula(days_since_1st_D_inj ~ Fat_Scores_Sum), lab)
-m4 = R"t.test(lab$days_since_1st_D_inj ~ lab$Diet)"
+m2 = lm(@formula(days_since_1st_D_inj ~ Fat_Scores_Sum), lab)
+m3 = R"t.test(lab$days_since_1st_D_inj ~ lab$Diet)"
+m4 = R"t.test(lab$days_since_1st_D_inj ~ lab$Sex)"
 m5 = R"chisq.test(lab$Diet, lab$Sex)"
-
-#weight and fat not independent given diet and sex, need to rework DAG
