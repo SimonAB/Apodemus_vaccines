7bf3d054d87cb9e35bb7de245c074454b4cec92a
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2020-12-02 23:55:37 +0000

    Update wild_vacc_analysis.jl
    Remove independence checks as this is now a new file

--- src/wild_vacc_analysis.jl
+++ src/wild_vacc_analysis.jl
@@ -22,14 +22,6 @@ lab =
   @filter(_.:Env == "Lab") |>
   DataFrame
 
-#test independences of current DAG
-@rput lab
-m1 = lm(@formula(Weight ~ days_since_1st_D_inj), lab)
-m2 = lm(@formula(days_since_1st_D_inj ~ Fat_Scores_Sum), lab)
-m3 = R"t.test(lab$days_since_1st_D_inj ~ lab$Diet)"
-m4 = R"t.test(lab$days_since_1st_D_inj ~ lab$Sex)"
-m5 = R"chisq.test(lab$Diet, lab$Sex)"
-
 #get model coefficients
 w1 = lm(@formula(Weight ~ Sex + Fat_Scores_Sum + Diet), lab)
 w2 = lm(@formula(Weight ~ Sex + Diet), lab)
