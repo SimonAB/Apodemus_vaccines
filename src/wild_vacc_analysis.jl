2b23dc75f76ca94c2316de4001843d624209c6ff
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2020-12-03 16:16:16 +0000

    Update wild_vacc_analysis.jl
    model weights

--- src/wild_vacc_analysis.jl
+++ src/wild_vacc_analysis.jl
@@ -23,13 +23,10 @@ lab =
   DataFrame
 
 #get model coefficients
-w1 = lm(@formula(Weight ~ Sex + Fat_Scores_Sum + Diet), lab)
-w2 = lm(@formula(Weight ~ Sex + Diet), lab)
-w3 = lm(@formula(Fat_Scores_Sum ~ Diet), lab)
-w4 = lm(@formula(Fat_Scores_Sum ~ Sex), lab)
-w5 = lm(@formula(OD ~ days_since_1st_D_inj + Sex + Diet), lab)
-w6 = lm(@formula(OD ~ Weight + Diet + days_since_1st_D_inj + Sex + Fat_Scores_Sum), lab)
-w7  = lm(@formula(OD ~ Fat_Scores_Sum + Diet + Sex + days_since_1st_D_inj), lab)
+w1 = lm(@formula(Weight ~ Diet + Sex), lab)
+w2 = lm(@formula(Weight ~ Fat_Scores_Sum + Diet + Sex), lab)
+w3 = lm(@formula(Fat_Scores_Sum ~ Sex + Diet), lab)
+w4 = lm(@formula(OD ~ days_since_1st_D_inj + Weight + Fat_Scores_Sum + Diet), lab)
 
 #wild mice OD over time curves
 wild =
