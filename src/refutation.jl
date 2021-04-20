98c60f8fdf36e8c6121bb9e372be3bbf4fde5121
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2021-04-20 19:32:40 +0100

    Update refutation.jl

--- src/refutation.jl
+++ src/refutation.jl
@@ -14,7 +14,16 @@ randnormalOD = DataFrame(randn(rng, Float64, (nrow(both), 1)))
 rename!(randnormalOD,:x1 => :randOD)
 randnormalOD.Row = (both.Row)
 
-
 # merge dummy response into new dataframe
 refute = copy(both; copycols=true)
 refute.randOD = (randnormalOD.randOD)
+
+# generate all-zero variable (placebo treatment) 
+placebo = DataFrame(Array{Union{Missing, Int}}(missing, nrow(both), 1))
+rename!(placebo,:x1 => :placebodiet)
+for col in names(placebo)
+   placebo[col] = Missings.coalesce.(placebo[col], 0)
+end
+
+# merge placebo treatment into refute dataframe
+refute.placebodiet = placebo.placebodiet
