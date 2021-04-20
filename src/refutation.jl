e0eb2b16bcc264394a721c438df2e4b3b509edbc
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2021-04-20 19:31:02 +0100

    Update refutation.jl

--- src/refutation.jl
+++ src/refutation.jl
@@ -8,10 +8,13 @@ import Missings
 # Import & filter data
 include("wild_vacc_data_cleanup.jl")
 
-# generate random normal response variable (dummy response) and merge into new dataframe
+# generate random normal response variable (dummy response) 
 rng = MersenneTwister(820480);
 randnormalOD = DataFrame(randn(rng, Float64, (nrow(both), 1)))
 rename!(randnormalOD,:x1 => :randOD)
 randnormalOD.Row = (both.Row)
+
+
+# merge dummy response into new dataframe
 refute = copy(both; copycols=true)
 refute.randOD = (randnormalOD.randOD)
