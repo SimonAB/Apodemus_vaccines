966444ddba0cdf752f903d5c0dfd5896c049abc5
Ewan Smith <63798599+ewanwsmith@users.noreply.github.com>
2021-05-28 14:38:30 +0100

    Update wild_vacc_analysis.jl

--- src/wild_vacc_analysis.jl
+++ src/wild_vacc_analysis.jl
@@ -28,18 +28,38 @@ p = plot(
 
 
 # combined DAG weights
+w1 = fit(MixedModel, @formula(Weight ~ Env + (1 | ID)), both)
+w2 = fit(MixedModel, @formula(Weight ~ Fat_Scores_Sum + Env + (1 | ID)), both)
+w3 = fit(MixedModel, @formula(Weight ~ Sex + Fat_Scores_Sum + (1 | ID)), both)
+w4 = fit(MixedModel, @formula(Weight ~ Diet + Env + (1 | ID)), both)
 
-fit(MixedModel, @formula(Weight ~ Env + (1|ID)), both)
-fit(MixedModel, @formula(Weight ~ Fat_Scores_Sum + Env + (1|ID)), both)
-fit(MixedModel, @formula(Weight ~ Sex + Fat_Scores_Sum + (1|ID)), both)
-fit(MixedModel, @formula(Weight ~ Diet + Env + (1|ID)), both)
+w5 = fit(MixedModel, @formula(Fat_Scores_Sum ~ Env + (1 | ID)), both)
+w6 = fit(MixedModel, @formula(Fat_Scores_Sum ~ Sex + Env + (1 | ID)), both)
+w7 = fit(
+    MixedModel,
+    @formula(Fat_Scores_Sum ~ days_since_1st_trt + Env + (1 | ID)),
+    both,
+)
 
-fit(MixedModel, @formula(Fat_Scores_Sum ~ Env + (1|ID)), both)
-fit(MixedModel, @formula(Fat_Scores_Sum ~ Sex + Env + (1|ID)), both)
-fit(MixedModel, @formula(Fat_Scores_Sum ~ days_since_1st_trt + Env + (1|ID)), both)
-
-fit(MixedModel, @formula(OD ~ Diet + Weight + days_since_1st_D_inj + Env + (1|ID)), both)
-fit(MixedModel, @formula(OD ~ Weight + Diet + Sex + Env + days_since_1st_D_inj + (1|ID)), both)
-fit(MixedModel, @formula(OD ~ Sex + Weight + days_since_1st_D_inj + (1|ID)), both)
-fit(MixedModel, @formula(OD ~ Env + Fat_Scores_Sum + days_since_1st_D_inj + (1|ID)), both)
-fit(MixedModel, @formula(OD ~ days_since_1st_D_inj + Env + (1|ID)), both)
+w8 = fit(
+    MixedModel,
+    @formula(OD ~ Diet + Weight + days_since_1st_D_inj + Env + (1 | ID)),
+    both,
+)
+w9 = fit(
+    MixedModel,
+    @formula(OD ~ Weight + Diet + Sex + Env + days_since_1st_D_inj + (1 | ID)),
+    both,
+)
+w10 = fit(
+    MixedModel,
+    @formula(OD ~ Sex + Weight + days_since_1st_D_inj + (1 | ID)),
+    both,
+)
+w11 = fit(
+    MixedModel,
+    @formula(OD ~ Env + Fat_Scores_Sum + days_since_1st_D_inj + (1 | ID)),
+    both,
+)
+w12 =
+    fit(MixedModel, @formula(OD ~ days_since_1st_D_inj + Env + (1 | ID)), both)
