7edd551b7eaacdd69043b860208f4933d43d8338
ewsmith1 <63798599+ewsmith1@users.noreply.github.com>
2021-07-09 13:09:22 +0100

    plot fixes

--- src/6_plots_for_writeup.jl
+++ src/6_plots_for_writeup.jl
@@ -10,9 +10,9 @@ include("1_data_import_cleanup.jl")
 # boxplot of OD by environment
 OD_Env_plot = plot(
     data,
-    x = :Env,
-    y = :logOD,
-    color = :Env,
+    x=:Env,
+    y=:logOD,
+    color=:Env,
     Geom.boxplot,
     Guide.ylabel("log(10) IgG1 OD"),
     Guide.xlabel("Environment")
@@ -23,9 +23,9 @@ OD_Env_plot |> PNG("plots/OD_Env_plot.png")
 # boxplot of OD by sex
 OD_Sex_plot = plot(
     data,
-    x = :Sex,
-    y = :logOD,
-    color = :Sex,
+    x=:Sex,
+    y=:logOD,
+    color=:Sex,
     Geom.boxplot,
     Guide.ylabel("log(10) IgG1 OD"),
     Guide.xlabel("Sex")
@@ -36,9 +36,9 @@ OD_Sex_plot |> PNG("plots/OD_Sex_plot.png")
 # boxplot of OD by diet
 OD_Diet_plot = plot(
     data,
-    x = :Diet,
-    y = :logOD,
-    color = :Diet,
+    x=:Diet,
+    y=:logOD,
+    color=:Diet,
     Geom.boxplot,
     Guide.ylabel("log(10) IgG1 OD"),
     Guide.xlabel("Diet")
@@ -49,13 +49,13 @@ OD_Diet_plot |> PNG("plots/OD_Diet_plot.png")
 # dot plot of OD by body mass
 OD_body_mass_plot = plot(
     data,
-    x = :Weight,
-    y = :logOD,
+    x=:Weight,
+    y=:logOD,
     Geom.point,
     Guide.ylabel("log(10) IgG1 OD"),
     Guide.xlabel("Body mass (g)"),
     Geom.line,
-    Stat.smooth(method = :lm)
+    Stat.smooth(method=:lm)
 )
 
 OD_body_mass_plot |> PNG("plots/OD_body_mass_plot.png")
@@ -63,10 +63,10 @@ OD_body_mass_plot |> PNG("plots/OD_body_mass_plot.png")
 # dot plot of OD by body mass
 OD_time_plot = plot(
     data,
-    x = :days_since_1st_D_inj,
-    y = :logOD,
+    x=:days_since_1st_D_inj,
+    y=:logOD,
     Geom.point,
-    color = :Env,
+    color=:Env,
     Guide.ylabel("log(10) IgG1 OD"),
     Guide.xlabel("time since vaccination (days)"),
 )
@@ -79,10 +79,26 @@ OD_time_plot |> PNG("plots/OD_time_plot.png")
 include("3_refutation.jl")
 
 # placebo treatment for diet
-placebo_4_plot = plot(placebo4, y = :Effect_size, x = :Edge, Geom.bar, color = :Edge)
+placebo_4_plot = plot(
+    placebo4, 
+    y=:Effect_size, 
+    x=:Edge, 
+    Geom.bar, 
+    color=:Edge,
+    Guide.ylabel("Effect Size"),
+    Guide.xlabel(nothing))
+
 placebo_4_plot |> PNG("plots/placebo_4_plot.png")
 
-placebo_8_plot = plot(placebo8, y = :Effect_size, x = :Edge, Geom.bar, color = :Edge)
+placebo_8_plot = plot(
+    placebo8, 
+    y=:Effect_size, 
+    x=:Edge, 
+    Geom.bar, 
+    color=:Edge,
+    Guide.ylabel("Effect Size"),
+    Guide.xlabel(nothing))
+    
 placebo_8_plot |> PNG("plots/placebo_8_plot.png")
 
 # dummy outcome treatment
